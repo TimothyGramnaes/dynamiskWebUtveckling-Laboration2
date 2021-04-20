a2bf440ce0fff37e29c25187625fab26b61625d2
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-04-20 11:42:46 +0200

    hejsan

--- Client/src/components/LandingPage/LandingPage.tsx
+++ Client/src/components/LandingPage/LandingPage.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 function LandingPage() {
     return(
+        <Grid container className="layoutMainContainer">
+            <Grid container xs={} md={} className="search"></Grid>
+
         <div>
             <h2>
                 hej
             </h2>
         </div>
+        </Grid>
     )
 }
 
