db6a78418b9077fdcd1fa0e7831b3a888122db7d
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-04-20 11:18:44 +0200

    Structural changes

--- Client/src/components/Layout.tsx
+++ Client/src/components/Layout.tsx
@@ -1,12 +1,15 @@
 import React from "react"
 import '../main.css'
 import Header from "./header/header"
+import LandingPage from "./LandingPage/LandingPage"
+
 
 function Layout() {
 
     return (
         <div>
             <Header />
+            <LandingPage />
             {/* // här har vi vår router */}
         </div>
     )
