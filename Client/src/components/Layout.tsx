2ce3ab7d291b8aaa4c563a22734b6518317cf51f
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2021-04-20 14:25:33 +0200

    added routing from front to admin

--- Client/src/components/Layout.tsx
+++ Client/src/components/Layout.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import "../main.css";
+import Admin from "./admin/Admin";
 import Header from "./header/header";
 import Hero from "./hero/Hero";
 
 function Layout() {
   return (
-    <div>
+    <BrowserRouter>      
       <Header />
-      <Hero />
+      <Switch>
+        <Route path="/admin/" component={Admin} />
+        <Route path="/" component={Hero} />
+      </Switch>
       {/* // här har vi vår router */}
-    </div>
+    </BrowserRouter>
   );
 }
 
