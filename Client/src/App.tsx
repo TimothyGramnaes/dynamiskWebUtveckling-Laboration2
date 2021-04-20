ab4fb7c6b946e71d98ec712f44252c0cef3c57d6
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2021-04-20 10:03:14 +0200

    hej hå

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Testar lite
+         Testar lite till hej hå
         </p>
         <a
           className="App-link"
