a8975d60e1fc20465af749c7f44f0761dd3c35dc
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-04-22 11:44:34 +0200

    first steps working

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const postRouter = require("./routers/post.router");
 
 const app = express();
 
-app.get;
+app.use(express.json());
+app.get("/", (req, res) => res.json("HÄÄÄÄÄÄÄÄÄÄJ!!!!"));
+app.use("/posts", postRouter);
+//server.use('/posts', postsRoutes)
 
-console.log("tja");
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json(err.message);
+});
+
+async function run() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/post", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("database is connected");
+  } catch (error) {
+    console.error(error);
+  }
+}
+run();
+
+app.listen(3001, () => {
+  console.log("Server is running");
+});
