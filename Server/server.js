c98f0246b845e3df288171c38d974002edbee6aa
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-04-27 16:16:35 +0200

    Fungerar

--- Server/server.js
+++ Server/server.js
@@ -2,26 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const postRouter = require("./routers/post.router");
 const userRouter = require("./routers/user.router");
-const cookieSession = require('cookie-session');
-const cookieParser = require('cookie-parser')
-const bcrypt = require('bcrypt');
+const cookieSession = require("cookie-session");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
 
 const app = express();
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(express.json());
 app.get("/", (req, res) => res.json("HÄÄÄÄÄÄÄÄÄÄJ!!!!"));
 app.use(postRouter);
 app.use(userRouter);
 
-app.use(cookieSession({
-  name: 'session',
-  secret: 'aVeryS3cr3tK3y',
-  secure: false,
-  maxAge: 1000 * 60,
-  httpOnly: true
-}));
-
+app.use(
+  cookieSession({
+    name: "session",
+    secret: "aVeryS3cr3tK3y",
+    secure: false,
+    maxAge: 1000 * 60,
+    httpOnly: true,
+  })
+);
 
 //server.use('/posts', postsRoutes)
 
@@ -32,30 +33,32 @@ app.use((err, req, res, next) => {
 
 async function run() {
   try {
-    await mongoose.connect("mongodb+srv://dbAdmin:dbAdmin@clusterdev.9j1cn.mongodb.net/salt", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://timpa:slarvkorv@cluster0.ddegw.mongodb.net/saltfactory",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("database is connected");
   } catch (error) {
     console.error(error);
   }
 }
 
-
 const auth = (req, res, next) => {
-  const ok = false
+  const ok = false;
 
   if (ok === false) {
-    res.send('Funkar inte')
+    res.send("Funkar inte");
   } else {
-    next()
+    next();
   }
-}
+};
 
-app.get('/admin', auth, (req, res) => {
-  res.send('Funkar')
-})
+app.get("/admin", auth, (req, res) => {
+  res.send("Funkar");
+});
 
 app.listen(3001, () => {
   console.log("Server is running");
