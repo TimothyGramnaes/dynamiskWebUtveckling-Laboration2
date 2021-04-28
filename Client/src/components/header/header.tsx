4f622d676209b66df172eae6b6f14a31b66a1796
Oliver Nygren <oliver.nygren@icloud.com>
2021-04-28 10:58:06 +0200

    routing

--- Client/src/components/header/header.tsx
+++ Client/src/components/header/header.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
 import './header.css'
 
 function Header() {
@@ -5,8 +7,12 @@ function Header() {
     <header>
       <h2 className="logo">Salt<b>Factory</b></h2>
       <div className="buttons">
+      <Link to="/login">
         <button className="log-in">Log In</button>
+      </Link>
+      <Link to="/signup">
         <button className="sign-up border-btn">Sign Up</button>
+      </Link>
       </div>
     </header>
   )
