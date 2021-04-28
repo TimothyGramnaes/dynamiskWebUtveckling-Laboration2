afc48555ea2f5d87612aa7a807803b25f5389e18
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2021-04-28 10:24:52 +0200

    Co-authored-by: David Jensen <Tsourdox@users.noreply.github.com>

--- Client/src/components/auth/AuthUser.tsx
+++ Client/src/components/auth/AuthUser.tsx
@@ -3,34 +3,33 @@ import  { Redirect, Route } from 'react-router-dom'
 import Admin from '../admin/Admin'
 
 function AuthUser({...rest}) {   
-    const [auth, setAuth] = useState<boolean>(false)
+    const [auth, setAuth] = useState<boolean>()
 
-    
-
-    const options = {
-        method: 'get'
-    }  
-
-    fetch('/api/user/auth', options)
-    .then(function (res){
-        if (res.status === 200) {
-            setAuth(res.ok)                   
-        } else {
-            setAuth(res.ok)
-        }        
-    })
-    .catch(function (err) {
-        console.log(err)
-    })   
-    
+    useEffect(() => {
+        const options = {
+            method: 'get'
+        }
+        fetch('/api/user/auth', options)
+        .then(function (res){
+            if (res.status === 200) {
+                setAuth(res.ok)                   
+            } else {
+                setAuth(res.ok)
+            }        
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
+    }, [setAuth])
 
 
-    
+    if (auth === undefined) {
+        return null;
+    }
 
     return (
         
         <Route {...rest} render={ () => {
-            console.log(auth)
             return auth === true
             ? <Admin />
             : <Redirect to="/login" />
