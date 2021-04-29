5bc4c0414a0a038f96a0473b39aa4b68f10ff7c6
Oliver Nygren <oliver.nygren@icloud.com>
2021-04-29 13:46:37 +0200

    code clean up - signup

--- Client/src/components/signUp/SignUp.tsx
+++ Client/src/components/signUp/SignUp.tsx
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import { Button } from "@material-ui/core";
-import {Link, useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import './signup.css'
-
 
 function SignUpComponent() {
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const history = useHistory()
-  
 
   const handleUserChange = (e:any) => {
     setEmail(e.target.value)
@@ -33,85 +31,80 @@ function SignUpComponent() {
           body: JSON.stringify(formData)
       }
       
-      fetch('/api/user/register', options
-      ).then((response)=>{
-        if (response.ok) {
-          history.push('/')
-        }
-        return response.text();
-      }).then((text) => {
-          console.log(text)
-      }).catch((error) => {
+      fetch('/api/user/register', options)
+        .then((response)=>{
+          if (response.ok) {
+            history.push('/')
+          }
+          return response.text();
+        })
+        .catch((error) => {
           console.log(error)
-      })
-          console.log(e)
+        })
   }
+
   return (
     <>
-
-  <div className="signup-container">
-
-    
-
-      <div className="form-container">
-
-
-    <form
-
-      action=""
-    >
-      <h3 className="create-account-title">CREATE ACCOUNT</h3>
-      <div style={{ width: "100%", display: "flex", flexDirection: "column" }}>
-      <TextField
-          style={{ margin: "1rem" }}
-          required
-          id="email"
-          label="Username"
-          defaultValue="username"
-          value={email}
-          onChange={handleUserChange}
-          // variant="outlined"
-        />
-        <TextField
-          style={{ margin: "1rem" }}
-          required
-          id="password"
-          type="password"
-          label="Password"
-          defaultValue="password"
-          value={password}
-          onChange={handlePasswordChange}
-          // variant="outlined"
-        />
-      </div>
-      <div className="signup-cancel-container">
-
-      <Button style={{ margin: "1rem" }} variant="outlined" color="primary"
-      type="submit"
-      onClick={handleClick}
-      >
-        SIGN UP
-      </Button>
-      <Button style={{ margin: "1rem" }} variant="outlined" color="secondary" href="/">
-        CANCEL
-      </Button>
-      </div>
-      <div className="smart-container">
-      <p style={{ padding: "1rem" }}>Already have an account?</p>
-      <Button
-        style={{ margin: "1rem" }}
-        variant="outlined"
-        color="primary"
-        href="/login"
-        >
-        LOG IN
-      </Button>
+      <div className="signup-container">
+        <div className="form-container">
+          <form>
+            <h3 className="create-account-title">CREATE ACCOUNT</h3>
+            <div style={{ width: "100%", display: "flex", flexDirection: "column" }}>
+              <TextField
+                style={{ margin: "1rem" }}
+                required
+                id="email"
+                label="Username"
+                defaultValue="username"
+                value={email}
+                onChange={handleUserChange}
+              />
+              <TextField
+                style={{ margin: "1rem" }}
+                required
+                id="password"
+                type="password"
+                label="Password"
+                defaultValue="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
+            <div className="signup-cancel-container">
+              <Button 
+                style={{ margin: "1rem" }} 
+                variant="outlined" 
+                color="primary"
+                type="submit"
+                onClick={handleClick}
+              >
+                SIGN UP
+              </Button>
+              <Button 
+                style={{ margin: "1rem" }} 
+                variant="outlined" 
+                color="secondary" 
+                href="/"
+              >
+                CANCEL
+              </Button>
+            </div>
+            <div className="smart-container">
+              <p style={{ padding: "1rem" }}>Already have an account?</p>
+              <Button
+                style={{ margin: "1rem" }}
+                variant="outlined"
+                color="primary"
+                href="/login"
+              >
+                LOG IN
+              </Button>
+            </div>
+          </form>
         </div>
-    </form>
+      </div>
 
-    </div>
-  </div>
-  </>
+    </>
   );
 }
 
