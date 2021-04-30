d66fd72f65f286f06b059e52ab27bb1b002f74ff
Oliver Nygren <oliver.nygren@icloud.com>
2021-04-30 15:37:25 +0200

    new title

--- Client/src/components/hero/Hero.tsx
+++ Client/src/components/hero/Hero.tsx
@@ -6,7 +6,7 @@ function Hero() {
     <div className="hero">
       <div className="text">
         <h1>Dont be salty, season with sugar</h1>
-        <h3>But please post your salts</h3>
+        <h3>But please post your salts, always anonymously</h3>
         <div className="posts">
           <LandingPage />
         </div>
